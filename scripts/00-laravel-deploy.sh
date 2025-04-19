079eaf966e8c87e07e17143081230dafa66a4b8d
yiying1007 <yiyingtan1007@gmail.com>
2025-04-19 18:10:52 +0800

    updated

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
- set -e
- echo "Running composer"
- composer global require hirak/prestissimo
- composer install --no-dev --working-dir=/var/www/html
- 
- echo "Caching config..."
- php artisan config:cache
- 
- echo "Caching routes..."
- php artisan route:cache
- 
- echo "Running migrations..."
- php artisan migrate --force
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
 
- echo "Building frontend assets..."
- npm install
- npm run build
- 
- echo "Checking Laravel logs if any errors..."
- cat storage/logs/laravel.log || echo "No Laravel error logs found."
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
